90363ee6dada6c04ae4de2a80648ea83f108ef99
irgndsondepp <robertschall@hotmail.com>
2017-05-31 17:16:45 +0200

    color tasks

--- people/tasks/cleanjob.go
+++ people/tasks/cleanjob.go
@@ -32,9 +32,25 @@ func (cj *Cleanjob) Name() string {
 }
 
 func (c *Cleanjob) ToString() string {
-	return fmt.Sprintf("%v (last done on %v)", c.Roomname, c.LastDone)
+	return fmt.Sprintf("%v (last done on %v)", c.Roomname, c.LastDone.Format(time.UnixDate))
 }
 
 func (c *Cleanjob) ToHtml() string {
-	return fmt.Sprintf("<p>%v (last done on %v)</p>", c.Roomname, c.LastDone)
+	return fmt.Sprintf("<p>%v</p>", c.colorLastDoneInHtml())
+}
+
+func (c *Cleanjob) colorLastDoneInHtml() string {
+	if c.isOverdue() {
+		return fmt.Sprintf("<font color=\"red\">%v</font>", c.ToString())
+	} else {
+		return fmt.Sprintf("<font color=\"green\">%v</font>", c.ToString())
+	}
+}
+
+func (c *Cleanjob) isOverdue() bool {
+	aWeekAgo := time.Now().AddDate(0, 0, -7)
+	if aWeekAgo.Before(c.LastDone) {
+		return false
+	}
+	return true
 }
