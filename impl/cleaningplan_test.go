0190ef59c071b57e93a6a855c64be43a0fdb3d2e
roberts <robertschall@hotmail.com>
2017-07-23 10:20:01 +0200

    Extend unit tests
    Added unit tests to check for:
    * Lowercase query
    * Error handling for not found task or assignee

--- impl/cleaningplan_test.go
+++ impl/cleaningplan_test.go
@@ -3,6 +3,8 @@ package impl
 import (
 	"testing"
 
+	"strings"
+
 	"github.com/irgndsondepp/cleaningplan/impl/mock"
 	"github.com/irgndsondepp/cleaningplan/interfaces"
 )
@@ -55,6 +57,67 @@ func TestRotateJobOutOfBounds(t *testing.T) {
 				t.Errorf("Expected job to have moved to Mock1: %v", task.GetAssignee())
 			}
 		}
+	}
+}
+
+func TestJobFoundForLowerCase(t *testing.T) {
+	mock1 := NewFlatmate("Dude")
+	task1 := createMockTask(mock1.GetName())
+	peops := []interfaces.Person{mock1}
+	tasks := []interfaces.Task{task1}
+	cp := NewRotatingCleaningPlan(mock.NewMockPersistence(&RotatingCleaningPlan{}))
+	cp.Init(peops, tasks)
+	dt := NewSimpleTask(strings.ToLower(task1.GetName()), mock1.GetName())
+	err := cp.MarkTaskAsDone(dt)
+	if err != nil {
+		t.Errorf("Unexpected error marking job as done: %v", err)
+	}
+	for _, task := range cp.Tasks {
+		if task.GetName() == task1.GetName() {
+			if task.GetAssignee() != task1.GetAssignee() {
+				t.Errorf("Wrong assignee for %v: expected %v, but was %v", task.GetName(), mock1.GetName(), task.GetAssignee())
+			} else if task.GetAssignee() != mock1.GetName() {
+				t.Errorf("Expected job to have moved to Mock1: %v", task.GetAssignee())
+			}
+		}
+	}
+	dt = NewSimpleTask(task1.GetName(), strings.ToLower(task1.GetAssignee()))
+	err = cp.MarkTaskAsDone(dt)
+	if err != nil {
+		t.Errorf("Unexpected error marking job as done: %v", err)
+	}
+	if len(cp.Tasks) != 1 {
+		t.Errorf("Expected task to be back in rotation")
+	}
+}
+
+func TestJobNotFound(t *testing.T) {
+	mock1 := NewFlatmate("Dude")
+	task1 := createMockTask(mock1.GetName())
+	task2 := createMockTask(mock1.GetName())
+	peops := []interfaces.Person{mock1}
+	tasks := []interfaces.Task{task1}
+	cp := NewRotatingCleaningPlan(mock.NewMockPersistence(&RotatingCleaningPlan{}))
+	cp.Init(peops, tasks)
+	err := cp.MarkTaskAsDone(task2)
+	if err == nil {
+		t.Errorf("Expected error marking job as done: %v", err)
+	}
+}
+
+func TestAssigneeNotFound(t *testing.T) {
+	mock1 := NewFlatmate("Dude")
+	mock2 := NewFlatmate("Dudette")
+	task1 := createMockTask(mock1.GetName())
+	task2 := createMockTask(mock2.GetName())
+	dt := NewSimpleTask(task2.GetName(), "noone")
+	peops := []interfaces.Person{mock1}
+	tasks := []interfaces.Task{task1}
+	cp := NewRotatingCleaningPlan(mock.NewMockPersistence(&RotatingCleaningPlan{}))
+	cp.Init(peops, tasks)
+	err := cp.MarkTaskAsDone(dt)
+	if err == nil {
+		t.Errorf("Expected error marking job as done: %v", err)
 	}
 }
 
